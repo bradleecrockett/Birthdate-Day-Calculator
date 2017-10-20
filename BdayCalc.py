82e495b7bcc2a7976bd619d7158613246c19b52a
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2017-10-19 21:58:12 -0700

    added tests

---
+++ BdayCalc.py
@@ -0,0 +1,41 @@
+# Name: First Last
+# Date:
+# Period:
+# Lab: BdayCalcComplete.py
+# Description: Uses Zeller's function to convert a birth date (Month, Day, Year)
+#     into the day of the week that the person was born.
+#
+#     Style - Code format, whitespace and PEP-8 style is followed making code easy to read.
+#     Comments - Blocks of code are well commented, every function has a descriptive comment.
+#     Tests -   The program runs as described in the specifications without errors(passes all tests).
+#       Takes user input to test the program in main()
+#       Uses the Zeller function to correctly calculate the day of the week that a user was born for any date
+
+import math
+
+
+def main():
+    '''
+    Takes user input (a date) and converts that
+    to date into a corresponding day of the week,
+    then prints the result
+    '''
+    # Take the user's birth month as input (int)
+
+    # Take the user's birth date as input
+
+    # Take the user's birth year as input
+
+    # Use your Zeller's Function defined below to calculate the birth day of the week
+
+    # Print the result above in the format: "You were born on a Monday!"
+
+
+def zeller_function(month: int, day: int, year: int):
+    '''Converts a date to a corresponding day of the week using Zeller's function'''
+    # Define the Zeller function below
+
+    return "Day of the Week"
+
+
+main()
---
+++ test_zeller_function.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+class TestZeller_function(TestCase):
+    pass
+
+    def test_zeller_function(self):
+        from BdayCalc import zeller_function
+        self.assertEqual(zeller_function(3, 10, 1984), "Saturday")
+        self.assertEqual(zeller_function(10, 19, 2017), "Thursday")
+        self.assertEqual(zeller_function(10, 20, 2017), "Friday")
+        self.assertEqual(zeller_function(7, 4, 1792), "Wednesday")
+        self.assertEqual(zeller_function(12, 31, 1999), "Friday")
+        self.assertEqual(zeller_function(1, 1, 2000), "Saturday")
+

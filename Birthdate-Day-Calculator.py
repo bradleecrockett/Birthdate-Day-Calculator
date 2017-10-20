342b22b28c1e155ac32eb81098449120fad426fe
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2017-10-19 19:20:13 -0700

    comments

---
+++ Birthdate-Day-Calculator.py
@@ -0,0 +1,49 @@
+'''
+Name: First Last
+Date:
+Period:
+Lab: Birthdate-Day-Calculator.py
+Description: Uses Zeller's function to convert a birthdate (Month, Day, Year)
+    into the day of the week that the person was born.
+
+    Style - code format, whitespace and PEP-8 style is followed making code easy to read.
+    Comments - blocks of code are well commented.  Every function has a descriptive comment.
+    Tests - The program runs as described in the specifications without errors(passes all tests).
+        Takes user input to test the program in main()
+        Uses the Zeller function to correctly calculate the day of the week that a user was born for any date
+
+'''
+
+'''
+Takes user input (a date) and converts that
+to date into a corresponding day of the week,
+then prints the result
+'''
+
+
+def main():
+
+
+# Take the user's birth month as input (int)
+
+# Take the user's birth date as input
+
+# Take the user's birth year as input
+
+
+# Use your Zeller's Function defined below to calculate the birth day of the week
+
+# Print the result above in the format: "You were born on a Monday!"
+
+
+
+main()
+
+'''Converts a date to a corresponding day of the week using Zeller's function'''
+
+
+def zeller_function(month: int, day: int, year: int):
+    # Define the Zeller function below
+    birth_day_of_week = ""
+
+    return birth_day_of_week
